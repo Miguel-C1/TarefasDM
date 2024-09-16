f4b88378a2ac6f794c9aa2f4596b2698f4e92f99
Miguel <doxito007@gmail.com>
2024-09-16 11:59:58 -0300

    Formatando código

--- provadm/lib/nota.dart
+++ provadm/lib/nota.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
 import 'package:provadm/url.dart';
 
 class TelaNotas extends StatefulWidget {
